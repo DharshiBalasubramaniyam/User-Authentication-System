bbb597516ce3ccdb325c6ca942b9224dc1d97ee1
Dharshib <Dharshib.8@gmail.com>
2023-08-26 10:25:54 +0530

    add styles

--- userDashboard/changePassword.php
+++ userDashboard/changePassword.php
@@ -33,7 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="../styles/styles.css">
     <title>Change Password - Coding.</title>
 </head>
 <body>
